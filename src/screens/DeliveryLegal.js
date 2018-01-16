6b5af5ed916c2827ea254a00fe927b1e51c68a02
Brian Cajulis <bcajulis12@apu.edu>
2018-01-16 13:23:49 -0800

    Sidebar Menu Scaff
    Screen Scaffold for the side bar menu screens & added eslint

---
+++ src/screens/DeliveryLegal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DeliveryLegal extends Component {
+    render() {
+        return (
+            <View>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+                <Text>DeliveryLegal Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default DeliveryLegal;
---
+++ src/screens/StandardLegal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class StandardLegal extends Component {
+    render() {
+        return (
+            <View>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+                <Text>StandardLegal Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default StandardLegal;
