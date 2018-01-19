3fe07ce0cb747caec38a56b656a902c33a4c99c7
Brian Cajulis <bcajulis12@apu.edu>
2018-01-19 12:33:46 -0800

    Test
    test

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { Button } from 'react-native-elements';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+
+import Navigate from './src/screens/Navigate';
+import CoffeePotList from './src/screens/CoffeePotList';
+import CoffeePot from './src/screens/CoffeePot';
+import ExistingOrdersList from './src/screens/ExistingOrdersList';
+import ExistingOrder from './src/screens/ExistingOrdersList';
 
 export default class App extends React.Component {
   render() {
+    const ButtonNav = StackNavigator({
+      Navigate: { screen: Navigate },
+      CoffeePotList: { screen: CoffeePotList },
+      CoffeePot: { screen: CoffeePot },
+      ExistingOrdersList: { screen: ExistingOrdersList },
+      ExistingOrder: { screen: ExistingOrder }
+    })
+
     return (
       <View style={styles.container}>
-        <Text>Hello Coffee Pot People!</Text>
+        <StatusBar
+          barStyle='light-content'
+        />
+        <ButtonNav />
       </View>
     );
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////
+// Style object
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
-  },
+    //marginTop: 25
+    marginTop: Platform.OS === 'android' ? 24 : 0
+  }
 });
---
+++ README
@@ -0,0 +1,2 @@
+# coffeepot
+Software engineering project for undergradute Senior Capstone (or course CS480) of 2018 for Azusa Pacific University.
