74a45908329c664cbda432b644ee72b4e07012b5
richnavarro <rnavarro15@apu.edu>
2018-01-19 11:56:14 -0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello Coffee Pot People!</Text>
+        <Text>Hello Coffee Pot People!!</Text>
       </View>
     );
   }
