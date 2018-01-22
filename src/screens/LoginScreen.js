5764de0edd5be6e27d9f147c6171e79028b2633e
bmiller15 <bmiller15@apu.edu>
2018-01-22 11:26:29 -0800

    Created base template for login and create account

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,1 +1,39 @@
-// Brenden Miller
+/*
+  Login Screen
+  Developers: Brenden Miller, Richard Navarro,
+    Brian Cajulis, Brett Arnold, Daniel Davis
+*/
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+///////////////////////////////////////////////////////////
+// Login Screen
+// Logs users into the application or directs them to signup
+// if they do not have an account
+///////////////////////////////////////////////////////////
+class LoginScreen extends Component {
+  static navigationOptions = {
+    title: 'Login',
+    headerStyle: {
+      backgroundColor: '#16A085'
+    },
+    headerTitleStyle: {
+      color: 'ECF0F1'
+    },
+    headerTintColor: 'white'
+  }
+  /////////////////////////////////////////////////////////
+  // Main render method
+  /////////////////////////////////////////////////////////
+  render() {
+    return (
+      <View>
+        <Text>
+          Login
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default LoginScreen;
