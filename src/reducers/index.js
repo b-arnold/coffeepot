46ea62b1cfb2b24cb030340233f535eca04b16e8
richnavarro <rnavarro15@apu.edu>
2018-03-15 15:12:06 -0700

    Update index.js
    Got rid of unwanted gitHub lines >>>>>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,8 @@ import { combineReducers } from 'redux';
 import AuthReducer from './auth_reducer';
 import CoffeePotReducer from './coffeepot_reducer';
 import PlacesReducer from './places_reducer';
-<<<<<<< HEAD
 import ProfileReducer from './profile_reducer';
-=======
 import OrderReducer from './order_reducer';
->>>>>>> Richard-Temp
 
 ////////////////////////////////////////////////////////////////////////
 // Combines reducers and assigns reducer names
@@ -14,9 +11,6 @@ export default combineReducers({
   auth: AuthReducer,
   coffee: CoffeePotReducer,
   places: PlacesReducer,
-<<<<<<< HEAD
-  prof: ProfileReducer
-=======
+  prof: ProfileReducer,
   order: OrderReducer
->>>>>>> Richard-Temp
 });
